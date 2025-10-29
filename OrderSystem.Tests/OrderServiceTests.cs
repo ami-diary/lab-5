00fdd8f0f626961ffe83e1a501799ae66ec684cf
Ami <168814388+ami-diary@users.noreply.github.com>
2025-10-30 01:10:13 +0300

    Add files via upload

--- OrderSystem.Tests/OrderServiceTests.cs
+++ OrderSystem.Tests/OrderServiceTests.cs
@@ -1,1 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderSystem;
 
+namespace OrderSystem.Tests
+{
+    [TestClass]
+    public class OrderServiceTests
+    {
+        [TestMethod]
+        public void CalculateOrderTotal_ReturnsCorrectSum()
+        {
+            var order = new Order();
+            order.AddProduct(new Product("Телефон", 30000));
+            order.AddProduct(new Product("Чехол", 1000));
+
+            var service = new OrderService();
+            var total = service.CalculateOrderTotal(order);
+
+            Assert.AreEqual(31000, total); // Проверка, что сумма корректна
+        }
+
+        [TestMethod]
+        public void IsOrderValid_ReturnsTrue_WhenProductsExist()
+        {
+            var order = new Order();
+            order.AddProduct(new Product("Монитор", 20000));
+
+            var service = new OrderService();
+            Assert.IsTrue(service.IsOrderValid(order)); // Проверка, что заказ с товарами валиден
+        }
+
+        [TestMethod]
+        public void IsOrderValid_ReturnsFalse_WhenEmptyOrder()
+        {
+            var order = new Order();
+            var service = new OrderService();
+            Assert.IsFalse(service.IsOrderValid(order)); // Проверка, что пустой заказ невалиден
+        }
+    }
+}
---
+++ OrderSystem.Tests/PaymentServicesTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderSystem;
+
+namespace OrderSystem.Tests
+{
+    [TestClass]
+    public class PaymentServiceTests
+    {
+        [TestMethod]
+        public void ProcessPayment_ReturnsTrue_WhenBalanceIsEnough()
+        {
+            var paymentService = new PaymentService();
+            Assert.IsTrue(paymentService.ProcessPayment(5000, 10000));
+        }
+
+        [TestMethod]
+        public void ProcessPayment_ReturnsFalse_WhenBalanceIsNotEnough()
+        {
+            var paymentService = new PaymentService();
+            Assert.IsFalse(paymentService.ProcessPayment(10000, 5000));
+        }
+
+        [TestMethod]
+        public void Integration_OrderService_With_PaymentService_WorksCorrectly()
+        {
+            var order = new Order();
+            order.AddProduct(new Product("Клавиатура", 3000));
+            order.AddProduct(new Product("Мышь", 1000));
+
+            var orderService = new OrderService();
+            var paymentService = new PaymentService();
+
+            var total = orderService.CalculateOrderTotal(order);
+            var paymentResult = paymentService.ProcessPayment(total, 10000);
+
+            Assert.IsTrue(paymentResult); // Проверка интеграции
+        }
+    }
+}
